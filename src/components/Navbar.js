6eceed51d3d581489a76f2c034b56139541b40fa
prerakkumarpatel <prerakkumar.patel@dcmail.ca>
2023-08-05 23:07:56 -0600

    changing navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,84 +31,44 @@ function NavBar() {
   window.addEventListener("scroll", scrollHandler);
 
   return (
-    <Navbar
-      expanded={expand}
-      fixed="top"
-      expand="md"
-      className={navColour ? "sticky" : "navbar"}
-    >
-      <Container>
-        {/* <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand> */}
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
+    <Nav className={"fixedleft"} className="ms-auto" defaultActiveKey="#home">
+      <Nav.Item>
+        <Nav.Link as={NavLink} to="/" onClick={() => updateExpanded(false)}>
+          <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+        </Nav.Link>
+      </Nav.Item>
+
+      <Nav.Item>
+        <Nav.Link
+          as={NavLink}
+          to="/about"
+          onClick={() => updateExpanded(false)}
         >
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto" defaultActiveKey="#home">
-            <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-              </Nav.Link>
-            </Nav.Item>
+          <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+        </Nav.Link>
+      </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/about"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
-              </Nav.Link>
-            </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          as={NavLink}
+          to="/project"
+          onClick={() => updateExpanded(false)}
+        >
+          <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} />
+          Projects
+        </Nav.Link>
+      </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/project"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />
-                Projects
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-            {/* 
-            <Nav.Item>
-              <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Nav.Item>
+        <Nav.Link
+          as={NavLink}
+          to="/resume"
+          onClick={() => updateExpanded(false)}
+        >
+          <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
 
