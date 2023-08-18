86c5765fafe9e080900472c61ab0e6de21bbd63a
prerakkumarpatel <prerakkumar.patel@dcmail.ca>
2023-08-18 08:19:11 -0600

    projects added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-// import leaf from "../../Assets/Projects/leaf.png";
-// import emotion from "../../Assets/Projects/emotion.png";
+import tarneeb from "../../Assets/Projects/tarneeb.png";
+import expensetracker from "../../Assets/Projects/expensetracker.png";
+import buber from "../../Assets/Projects/buber.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-// import suicide from "../../Assets/Projects/suicide.png";
-// import bitsOfCode from "../../Assets/Projects/blog.png";
+import dbas from "../../Assets/Projects/dbas.png";
+import blog from "../../Assets/Projects/blog.png";
+
+import hackernews from "../../Assets/Projects/hackernews.png";
 
 function Projects() {
   return (
@@ -26,22 +29,22 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Wiscord"
-              description="Personal Server Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="Wiscord revolutionizes personal server rooms with integrated end-to-end messaging. Elevate communication and collaboration within Chatify, inspired by Soumyajit's vision. Seamlessly connect, share, and create together ."
               ghLink="https://github.com/prerakkumarpatel/wiscord-app"
               demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={hackernews}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Hacker News GraphQL"
+              description="Crafted Hacker News GraphQL using React and GraphQL for API . Enhanced data retrieval and interactive user experience, providing streamlined access to trending tech stories and discussions from Hacker News.Allowing users to vote , add , edit, remove any stories"
+              ghLink="https://github.com/prerakkumarpatel/hackernews-react-apolloGraphQL"
+              // demoLink="https://blogs.soumya-jit.tech/"
             />
-          </Col> */}
+          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -50,43 +53,62 @@ function Projects() {
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               ghLink="https://github.com/prerakkumarpatel/Editor.io-master"
-              demoLink="https://editor.soumya-jit.tech/"
-            />
-          </Col>
-          {/* 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              demoLink="https://codeeditor-5qqh.onrender.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={expensetracker}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Expense Tracker"
+              description="Empower financial management with React-based Expense Tracker. Visualize spending patterns through interactive UI, enabling users to monitor and optimize expenses for a healthier financial outlook"
+              ghLink="https://github.com/prerakkumarpatel/expense-tracker-react"
+              demoLink="https://expense-tracker-react.onrender.com/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={tarneeb}
+              isBlog={false}
+              title="Tarneeb (Trump Card)"
+              description="Introducing CardQuest: A captivating card game crafted with C# for a robust backend, WPF for a visually stunning UI, and a paramount emphasis on Object-Oriented Programming (OOP). Immerse yourself in strategic gameplay and a seamless gaming experience."
+              ghLink="https://github.com/Zeel-Sutariya/TarneebCardGame"
+              // demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={dbas}
+              isBlog={false}
+              title="Lackridge Hospital Management System"
+              description="Employing SDLC, client requirements were gathered for 'Control Centre of Neo.' Utilized C# backend, WPF UI, and OOP principles. Designed database and dashboard for seamless data access and management"
+              ghLink="https://github.com/prerakkumarpatel/"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={blog}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Blog"
+              description="Built a dynamic blog site using js, offering engaging UI and seamless navigation. Integrated features for posts, comments, and user interaction to enhance the blogging experience."
+              ghLink="https://github.com/prerakkumarpatel/ejs-challenge-final"
+              demoLink="https://blog-page-ud8f.onrender.com/posts/Patel"
             />
-          </Col> */}
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={buber}
+              isBlog={false}
+              title="Tomato"
+              description="Developed Tomato Food Delivery app using React and Firebase. Seamless ordering experience, real-time updates, and secure transactions make it a reliable platform for food enthusiasts"
+              ghLink="https://github.com/prerakkumarpatel/food-ordering-app-react"
+              demoLink="https://buber-eats-order-anytime.onrender.com/"
+            />
+          </Col>
         </Row>
       </Container>
     </Container>
